569ae319a97c36089f6a223994549fae984e575c
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-19 15:25:07 +0600

    design & add root color

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,10 +9,10 @@ const Navbar = () => {
       <nav style={{backgroundColor: "aqua", padding: "15px", borderRadius: "10px"}}>
       <div className="nav-center d-flex justify-content-between align-items-center">
         <h3 style={{cursor: "pointer"}}>hotDeals</h3>
-        <div className="nav-container d-flex">
+        <div className="nav-container d-block position-relative">
           <img src={bag} alt="bag" />
           <div className="amount-container">
-            <p className="total-amount p-2 fs-4"> <sup>5</sup></p>
+            <p className="total-amount text-info fw-bold fs-5"> 0 </p>
           </div>
         </div>
       </div>
