5acdf087e5563b0927f7df81d1e466a55d5c2af4
nazmul162001 <nazmulprime162001@gmail.com>
2022-03-21 10:52:09 +0600

    test button click

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import bag from '../../media/icons/bag.svg';
+import Products from '../Products/Products';
 
 const Navbar = () => {
+  const [count, setCouont] = useState(0);
+  // const add = () => {
+  //   setCouont(count + 1)
+  // }
   return (
     <div>
       <div className="container my-5">
@@ -12,11 +17,16 @@ const Navbar = () => {
         <div className="nav-container d-block position-relative">
           <img src={bag} alt="bag" />
           <div className="amount-container">
-            <p className="total-amount text-info fw-bold fs-5"> 0 </p>
+            <p className="total-amount text-info fw-bold fs-5"> {count} </p>
           </div>
         </div>
       </div>
       </nav>
+      </div>
+      <Products></Products>
+      <div className="container">
+      <button className='m-3' onClick={() => setCouont(count+1)}>Add</button>
+      <button onClick={() => count <= 0 ? 0 :  setCouont(count-1)}>Delete</button>
       </div>
     </div>
   );
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
 const Products = () => {
   return (
     <div className="container">
       <div>
           <h2>This is my product section</h2>
+          {/* <button onClick={'add'} className='m-3'>Add Product</button>
+          <button>Delete Product</button> */}
       </div>
     </div>
   );
